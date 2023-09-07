113d5782f4d76f0b4705893be97d2e73c81b934d
neiteel <51899861+neiteel@users.noreply.github.com>
2023-09-07 10:53:46 +0800

    init

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 80,
+  tailwindFunctions: ['clsx'],
+  plugins: [
+    'prettier-plugin-tailwindcss', // MUST come last
+  ],
+};
